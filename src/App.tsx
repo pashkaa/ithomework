256fb262bb44d4d1a6c9de44be3d2a4e462bceec
Pavlo <pashaleet@gmail.com>
2023-12-18 00:18:02 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import s from "./App.module.css"
+import { createBrowserRouter, createRoutesFromElements, Route, Link, RouterProvider, BrowserRouter, Routes } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { Data } from './components/Data/Data';
+import { TopMenu } from './components/TopMenu/TopMenu';
+import { LeftBlock } from './components/LeftBlock/LeftBlock';
+import { Products } from './components/products/Products';
+import { Orders } from './components/Orders/Orders';
+import { Groups } from "./components/Groups/Groups"
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <TopMenu />
+        <div className={s.container}>
+          <LeftBlock />
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/products' element={<Products />}></Route>
+            <Route path='/groups' element={<Groups />}></Route>
+            <Route path='/orders' element={<Orders />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
+
 
 export default App;
